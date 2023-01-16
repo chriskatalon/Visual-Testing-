529fbdedfdcd23e72e00e696a6889198c9a566c5
chriskatalon <99629253+chriskatalon@users.noreply.github.com>
2023-01-16 21:10:35 +0700

    haha

--- Test Suites/TS3 - QK.ts
+++ Test Suites/TS3 - QK.ts
@@ -5,10 +5,17 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>da2e3dbb-3aff-4bd2-929a-07c1749a47b3</testSuiteGuid>
+   <testCaseLink>
+      <guid>b538de7b-636b-4790-8d72-b476c8cf6440</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC3 - QK</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
